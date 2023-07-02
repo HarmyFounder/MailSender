909c5cf895b0eadb75e72aeb74bb20119b29ba5c
Maksim Efimchick <maksefimchic@gmail.com>
2023-07-03 00:12:29 +0300

    fixed text

--- src/java/com/HarmyFounder/Mail/MailSender.java
+++ src/java/com/HarmyFounder/Mail/MailSender.java
@@ -34,8 +34,8 @@ public class MailSender {
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(from));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-            message.setSubject("Продуктивность");
-            message.setText("Everyone needs a little push sometimes. Whether you're starting out on a new career path or need a burst of motivation, these inspiring words will remind you that success is possible. Plus, check out more powerful quotes that will get you in a positive mindset.");
+            message.setSubject("Message Tag");
+            message.setText("Message Text");
             Transport.send(message);
         } catch(Exception e) {
             e.printStackTrace();
